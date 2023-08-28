0c49319d0e42956b49eeb98ca966ecc7836b8f35
mujahir2023 <126049353+mujahir2023@users.noreply.github.com>
2023-08-28 14:23:18 +0530

    push

--- src/app/header&footer/footer/footer.component.ts
+++ src/app/header&footer/footer/footer.component.ts
@@ -24,7 +24,7 @@ export class FooterComponent {
 
     const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 
-    console.log('[scroll]', scrollPosition);
+    // console.log('[scroll]', scrollPosition);
 
     if (scrollPosition >= this.topPosToStartShowing) {
       this.isShow = true;
